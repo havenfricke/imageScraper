039b8bdc96763a256c7f0c1551c08c0987861d38
Haven Fricke <97204109+havenfricke@users.noreply.github.com>
2023-01-12 09:36:31 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+
+(async () => {
+    const browser = await puppeteer.launch({
+        args: [
+            '--window-size=1920, 1080',
+        ],
+    });
+   
+    const page = await browser.newPage();
+
+    //set the viewport in which the images will be viewed and saved.
+    await page.setViewport({
+        width: 1920,
+        height: 1080,
+      });
+
+
+    page.on('response', async (response) => {
+        
+        //specify which file extension to look for
+      const matches = /.*\.(jpg|png|gif|mp4|webp|webm|jpeg)$/.exec(response.url());
+
+        //get the filename
+      const matchName = {...matches};
+      let fileName = matchName[0];
+      let stringName = String(fileName);
+      let convertedName = stringName.split('/').pop();
+      console.log(matches, convertedName);
+
+      //store images from webpages
+      if (matches && (matches.length === 2)) {
+        const buffer = await response.buffer();
+        fs.writeFileSync(`images/${convertedName}`, buffer, 'base64');
+      }
+    });
+
+        //desired page to scrape
+    await page.goto('https://put-a-website-url-here.com');
+
+    await browser.close();
+})();
